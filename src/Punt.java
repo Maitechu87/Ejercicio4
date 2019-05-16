2481c63d35f781fda5fee9a38a63aa542c7d6d09
Maitechu87 <maitechu87@gmail.com>
2019-05-16 18:17:45 +0200

    Exercici 7 aprofitant l'encapsulació

--- src/Punt.java
+++ src/Punt.java
@@ -1,32 +1,36 @@
 public class Punt {
 
-    private int x;
-    private int y;
+    private int[] coord;
 
+    public Punt(int x, int y){
+        coord = new int [2];
+        coord[0] = x;
+        coord[1] = y;
+    }
 
     public int getX() {
-        return x;
+        return coord[0];
     }
 
     public int getY(){
-        return y;
+        return coord[1];
     }
 
     public void setX(int x) {
-        this.x = x;
+        this.coord[0] = x;
     }
 
     public void setY(int y) {
-        this.y = y;
+        this.coord[1] = y;
     }
 
     public Punt() { this(0, 0); }
 
-    public Punt(int x, int y) { this.x=x; this.y=y; }
+    //public Punt(int x, int y) { this.x=x; this.y=y; }
 
     public void suma(Punt p) {
-        x+=p.x;
-        y+=p.y;
+        coord[0]+=p.coord[0];
+        coord[1]+=p.coord[1];
     }
 
 
